c38f94402f386649555924002703d86031b92083
Dreamweaver96 <wojciech.b96@gmail.com>
2024-07-22 23:06:21 +0200

    Added .idea IDE folder to .gitignore to remove it from git

--- losowanie.py
+++ losowanie.py
@@ -2,28 +2,28 @@ import random
 import time
 
 
-def losowanie():
-    dostepne_liczby = list(range(1, 50))
-    wylosowane_liczby = []
-    counterek = 0
+def draw():
+    available_numbers = list(range(1, 50))
+    drawn_numbers = []
+    draw_counter = 0
 
 
-    while counterek < 6:
-        #time.sleep(1.5)
-        wylosowana_liczba = random.choice(dostepne_liczby)
-        print(wylosowana_liczba)
-        print(dostepne_liczby)
-        dostepne_liczby.remove(wylosowana_liczba)
-        wylosowane_liczby.append(wylosowana_liczba)
-        print(f"Wylosowano liczbe " + str(wylosowana_liczba))
-        print(dostepne_liczby)
-        counterek = counterek + 1
+    while draw_counter < 6:
+        time.sleep(1.5)
+        drawn_number = random.choice(available_numbers)
+        print(drawn_number)
+        #print(available_numbers)
+        available_numbers.remove(drawn_number)
+        drawn_numbers.append(drawn_number)
+        print(f"Drawn number is " + str(drawn_number))
+        #print(available_numbers)
+        draw_counter = draw_counter + 1
 
     time.sleep(1.5)
-    wylosowane_liczby.sort()
-    #str(wylosowane_liczby)
-    #print(wylosowane_liczby)
-    '''print(f"Wylosowane liczby to: " + str(wylosowane_liczby))'''
-    print("Wylosowane liczby to: " + ", ".join(map(str, wylosowane_liczby)))
+    drawn_numbers.sort()
+    #str(drawn_numbers)
+    #print(drawn_numbers)
+    '''print(f"Drawn number is: " + str(drawn_numbers))'''
+    print("Drawn numbers are: " + ", ".join(map(str, drawn_numbers)))
 
-losowanie()
+draw()
