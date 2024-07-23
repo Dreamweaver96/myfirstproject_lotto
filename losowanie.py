18b7e795cdb9556d4e215c9be98ce8e49175e53c
Dreamweaver96 <wojciech.b96@gmail.com>
2024-07-23 14:52:47 +0200

    added new class Lottery and new function SelectionEvent

--- losowanie.py
+++ losowanie.py
@@ -1,11 +1,52 @@
 import random
 import time
 
+class Lottery:
+    def __init__(self):
+        self.available_numbers = list(range(1, 50))
+        self.selected_numbers = []
 
+def SelectionEvent():
+    selection = Lottery()
+    while len(selection.selected_numbers) < 6:
+        try:
+            selected_number = int(input("Please select a number from 1 to 49\n"))
+        except:
+            print("Incorrect input. Please enter an integer.")
+        print(selected_number)
+        print(type(selected_number))
+        if selected_number not in selection.available_numbers and selected_number not in selection.selected_numbers:
+            print("Selected number is out of range. Please choose an integer from 1 to 49")
+        elif selected_number in selection.selected_numbers:
+            print("You have already chosen this number. Please type another number.")
+        else:
+            selection.selected_numbers.append(selected_number)
+            selection.available_numbers.remove(selected_number)
+    print("dziala normalnie waldusiu")
+def DrawEvent():
+    draw = Lottery()
+    while len(draw.selected_numbers) < 6:
+        time.sleep(1.5)
+        drawn_number = random.choice(draw.available_numbers)
+        print(drawn_number)
+        draw.available_numbers.remove(drawn_number)
+        draw.selected_numbers.append(drawn_number)
+        print(f"Drawn number is " + str(drawn_number))
+
+    time.sleep(1.5)
+    draw.selected_numbers.sort()
+    print("Drawn numbers are: " + ", ".join(map(str, draw.selected_numbers)))
+
+SelectionEvent()
+DrawEvent()
+
+
+"""This is old code containing only draw function, without lottery class and with unnecessary counter "draw_counter"
 def draw():
-    available_numbers = list(range(1, 50))
+    NumbersRange.available_numbers = list(range(1, 50))
     drawn_numbers = []
     draw_counter = 0
+    print(available_numbers)
 
 
     while draw_counter < 6:
@@ -26,4 +67,6 @@ def draw():
     '''print(f"Drawn number is: " + str(drawn_numbers))'''
     print("Drawn numbers are: " + ", ".join(map(str, drawn_numbers)))
 
-draw()
+draw()"""
+
+
